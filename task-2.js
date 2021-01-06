204905c0568238cb30d8c375830571fce6d48c8b
Alexandr Volokh <angelus72008@gmail.com>
2021-01-06 15:27:14 +0200

    hw-03

--- task-2.js
+++ task-2.js
@@ -1,20 +1,10 @@
-const countProps = function(obj) {
-    const array = Object.keys(obj);
-    console.log(array);
-    // return array.length,
-    const array2 = Object.values(obj);
-    console.log(array2);
-    // return array2.length,
-    const array3 = Object.entries(obj);
-    console.log(array3);
-    // return array3.length
-  };
-  
- 
+const countProps = function (obj) {
+  const keysArray = Object.keys(obj);
+  return keysArray.length;
+}
 
+console.log(countProps({})); // 0
 
-  console.log(countProps({})); // 0
-  
-  console.log(countProps({ name: 'Mango', age: 2 })); // 2
-  
-  console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
